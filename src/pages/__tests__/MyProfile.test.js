74ae42f15ea7babf32224588623db040b54bb3db
aliatayee <aliaqaatayee@gmail.com>
2022-07-22 14:11:30 +0430

    Add test for rockets

--- src/pages/__tests__/MyProfile.test.js
+++ src/pages/__tests__/MyProfile.test.js
@@ -1,9 +1,29 @@
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import store from '../../redux/configureStore';
 import MyProfile from '../MyProfile/MyProfile';
 
-it('Button component render', () => {
-  const tree = renderer.create(<Provider store={store}><MyProfile /></Provider>).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Test my profile', () => {
+  it('Button component render', () => {
+    const tree = renderer.create(<Provider store={store}><MyProfile /></Provider>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('Test My Rockets title', () => {
+    render(
+      <Provider store={store}>
+        <MyProfile />
+      </Provider>,
+    );
+    expect(screen.getByRole('heading', { name: /My Rockets/i })).toBeInTheDocument();
+  });
+  it('Test My Missions title', () => {
+    render(
+      <Provider store={store}>
+        <MyProfile />
+      </Provider>,
+    );
+    expect(screen.getByRole('heading', { name: /My Missions:/i })).toBeInTheDocument();
+  });
 });
